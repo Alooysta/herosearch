f30dedfa68ccf781b17df28b466808631395111e
Guilherme de Sousa Santos <114372799+Alooysta@users.noreply.github.com>
2023-08-24 15:02:23 -0300

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,45 +1,34 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 5000;
-const hostname = "localhost"
-const axios = require('axios')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const crypto = require('crypto')
+const port = process.env.PORT || 5000;  // This will work for Render as well
 
-app.use(cors())
-app.use(bodyParser.urlencoded({extended: false }))
-app.use(bodyParser.json())
+import { get } from 'axios';
+import cors from 'cors';
+import { urlencoded, json } from 'body-parser';
+import { createHash } from 'crypto';
 
-
-
+app.use(cors());
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 const ts = new Date().getTime();
+const privateKey = process.env.PRIVATE_KEY;
+const publicKey = process.env.PUBLIC_KEY;
+const hash = createHash('md5').update(ts + privateKey + publicKey).digest('hex');
 
-//precisa ser nessa ordem, não esquece :TS, PRIVATEKEY,PUBLICKEY
-console.log(ts)
-const privateKey = process.env.PRIVATE_KEY
-const publicKey = process.env.PUBLIC_KEY
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}`);
+});
 
+app.post('/', async (req, res) => {
+    const { character } = req.body;
+    const info = await get(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${character}&ts=${ts}&apikey=${publicKey}&hash=${hash}`);
+    res.send(info.data);
+});
 
-const hash = crypto.createHash('md5').update(ts+privateKey+publicKey).digest('hex')
-
-
-app.listen(port, hostname, () => {
-
-    console.log(`O server está rodando em ${hostname} ${port}`)
-})
-
-
-app.post('/', async(req, res) => {
-    const{character} = req.body
-    const info = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${character}&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-    res.send(info.data)
-})
-
-app.post('/characterinfo', async(req, res) => {
-    const {id} = req.body
-    const infoId = await axios.get(`https://gateway.marvel.com:443/v1/public/characters?id=${id}&ts=${ts}&apikey=${publicKey}&hash=${hash}`)
-    res.send(infoId.data)
-})
+app.post('/characterinfo', async (req, res) => {
+    const { id } = req.body;
+    const infoId = await get(`https://gateway.marvel.com:443/v1/public/characters?id=${id}&ts=${ts}&apikey=${publicKey}&hash=${hash}`);
+    res.send(infoId.data);
+});
